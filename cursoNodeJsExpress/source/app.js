26b28ada5825bb5d706066e392427dee879f0b3f
Bruno Mendes <brunomendes962@gmail.com>
2021-01-30 19:00:41 -0200

    aula 09 curso node express

--- cursoNodeJsExpress/source/app.js
+++ cursoNodeJsExpress/source/app.js
@@ -1,39 +1,14 @@
 const express = require('express')
 
 const app = express()
+app.set('view engine', 'ejs')
 
 app.get('/', function(request, response){
-  response.send(` 
-    <!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">      
-      <title>Document</title>
-    </head>
-    <body>
-      <h1> Pagina home </h1>
-    </body>
-    </html>
-  `)
+  response.render('site/home')
 })
 
 app.get('/contato', function (request, response) {
-  response.send(` 
-    <!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">      
-      <title>Document</title>
-    </head>
-    <body>
-      <h1> Pagina contato </h1>
-    </body>
-    </html>
-  `)
+  response.render('site/contato')
 })
 
 
