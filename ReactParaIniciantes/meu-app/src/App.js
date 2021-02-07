1b486753d7d70ba45dd29ead2f74ed25da8c869e
Bruno Mendes <brunomendes962@gmail.com>
2021-02-07 16:59:51 -0200

    React Origamid: Fim Sesssao 2

--- ReactParaIniciantes/meu-app/src/App.js
+++ ReactParaIniciantes/meu-app/src/App.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import Footer  from './Footer'
+
 import Form from './form/Form'
-import Header from './Header'
 
 const App = () => {  
-  function handleClick(event) {
-    console.log(event.target)
-  }
-
   return (
     <>
       <Form />
