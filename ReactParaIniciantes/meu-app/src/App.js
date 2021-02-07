a21c9638f24593878acfaf164557a3f6756d219a
Bruno Mendes <brunomendes962@gmail.com>
2021-02-07 17:38:29 -0200

    React Hooks Aula 1

--- ReactParaIniciantes/meu-app/src/App.js
+++ ReactParaIniciantes/meu-app/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-import Form from './form/Form'
-
 const App = () => {  
+  // useState Hook
+  const [ativo, setAtivo] = React.useState(false)
+  const [dados, setDados] = React.useState({nome: 'Bruno', idade: 24})
+  
+  function handleClick(){
+    setAtivo(!ativo)
+    setDados({...dados, faculdade: 'fez faculdade'})
+  }
+
   return (
     <>
-      <Form />
+      <p>{dados.nome}</p>
+      <p>{dados.idade}</p>
+      <p>{dados.faculdade}</p>
+      <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'}</button>
     </>
   )
 }
---
+++ ReactParaIniciantes/meu-app/src/ButtonModal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ButtonModal
