27539ce0a38360dab0cfb5f7c2351cc8cc1f0e57
Bruno Mendes <brunomendes962@gmail.com>
2021-02-09 12:05:12 -0200

    React css Modules

--- ReactParaIniciantes/meu-app/src/App.js
+++ ReactParaIniciantes/meu-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Produto from './CssModule/Produto'
 import FormularioApp from './Formularios/FormularioApp'
+import './style.css'
 
 const App = () => {    
   
     return (
       <>
         <div>
-          <FormularioApp />
+          <Produto />
         </div>
       </>
     )
---
+++ ReactParaIniciantes/meu-app/src/CssModule/Produto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Produto.module.css'
+
+const Produto = () => {
+  return (
+    <div>
+      <h1 className={style.titulo}>aaa</h1>
+    </div>
+  )
+}
+
+export default Produto
